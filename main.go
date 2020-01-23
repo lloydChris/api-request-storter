09ee12869bfcf88575b23a1d1db86ba9e166bc3b
Chris Johnson <cjohnson@gasbuddy.com>
2020-01-23 14:20:03 -0600

    Add main file and comments describing rought outline of program flow

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting execution...")
+	//read in command line params
+
+	//Check if the input file exists
+
+	//Read one line
+
+	//Parse out just the Method and path (case insensitive)
+
+	//store in Key-value store with value 1 or increment existing occurrence
+
+}
